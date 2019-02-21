bcc8dda9a04812695c5277c1778f4c1229970b07
Your Name <345111458@qq.com>
2019-02-21 10:01:57 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,8 +7,8 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
+    public function create(){
+
         return view('users.create');
     }
 
@@ -24,9 +24,21 @@ class UsersController extends Controller
 
         $this->validate($request , [
             'name'  => 'required|max:50',
-            'email' => 'required|email|uniqid:users|max:255',
+            'email' => 'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
-        return;
+
+
+        $user = User::create([
+            'name'  =>  $request->name,
+            'email' =>  $request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show',[$user]);
     }
+
+
 }
