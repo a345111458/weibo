01b0568af2a76a94c6aa4c39d6275425f3d9cc50
Your Name <345111458@qq.com>
2019-03-08 09:51:37 +0000

    用户激活

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run(){
         //
-        $users = factory(User::class)->times(9050)->make();
+        $users = factory(User::class)->times(250)->make();
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
